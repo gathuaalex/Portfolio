eaeb5643b688e7290ac0696a5a1a781a2e1d5555
Alex Gathua <64085923+gathuaalex@users.noreply.github.com>
2020-08-24 20:10:25 +0300

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+                          document_root=settings.STATICFILES_STORAGE)
